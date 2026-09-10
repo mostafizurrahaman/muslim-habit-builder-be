4e0ed58a4c3e7d60a4bca9a40757fc6b12a3def4
mostafizurrahaman <mostafizurrahaman0401@gmail.com>
2026-09-10 15:14:31 +0600

    feat: made account retrieved api public

--- src/app/modules/dashboard/announcement/announcement.route.ts
+++ src/app/modules/dashboard/announcement/announcement.route.ts
@@ -1,49 +1,41 @@
-import { Router } from "express";
+import { Router } from 'express';
 
-import { announcementController } from "../announcement/announcement.controller";
-import { USER_ROLE } from "../../user/user.constant";
-import authMiddleware from "../../../middlewares/auth.middleware";
-import { validateRequest } from "../../../middlewares/request.validator";
-import announcementValidationZodSchema from "./announcement.zod";
-
-
-
-
-
+import { announcementController } from '../announcement/announcement.controller';
+import { USER_ROLE } from '../../user/user.constant';
+import authMiddleware from '../../../middlewares/auth.middleware';
+import { validateRequest } from '../../../middlewares/request.validator';
+import announcementValidationZodSchema from './announcement.zod';
 
 const announcementRouter = Router();
 
 announcementRouter.post(
-    '/add',
-    authMiddleware(USER_ROLE.SUPER_ADMIN, USER_ROLE.ADMIN),
-    validateRequest({
-       body: announcementValidationZodSchema.announcementSchema
-    }),
-    announcementController.createAnnouncementIntoDb,
+  '/add',
+  authMiddleware(USER_ROLE.SUPER_ADMIN, USER_ROLE.ADMIN),
+  validateRequest({
+    body: announcementValidationZodSchema.announcementSchema,
+  }),
+  announcementController.createAnnouncementIntoDb,
 );
 
-
-
 announcementRouter.patch(
-    '/update/:id',
-    authMiddleware(USER_ROLE.ADMIN, USER_ROLE.SUPER_ADMIN),
-    validateRequest({
-        body: announcementValidationZodSchema.updateAnnouncementSchema,
-    }),
-    announcementController.updateAnnouncementIntoDb,
+  '/update/:id',
+  authMiddleware(USER_ROLE.ADMIN, USER_ROLE.SUPER_ADMIN),
+  validateRequest({
+    body: announcementValidationZodSchema.updateAnnouncementSchema,
+  }),
+  announcementController.updateAnnouncementIntoDb,
 );
 
 announcementRouter.delete(
-    '/delete/:id',
-    authMiddleware(USER_ROLE.ADMIN, USER_ROLE.SUPER_ADMIN),
-    announcementController.deleteAnnouncementIntoDb,
+  '/delete/:id',
+  authMiddleware(USER_ROLE.ADMIN, USER_ROLE.SUPER_ADMIN),
+  announcementController.deleteAnnouncementIntoDb,
 );
 
 announcementRouter.get(
-    '/retrieve',
-    authMiddleware(USER_ROLE.ADMIN, USER_ROLE.SUPER_ADMIN),
-    announcementController.getAllAnnouncementsForAdmin,
+  '/retrieve',
+  // authMiddleware(USER_ROLE.ADMIN, USER_ROLE.SUPER_ADMIN),
+  announcementController.getAllAnnouncementsForAdmin,
 );
-
 
 export default announcementRouter;
